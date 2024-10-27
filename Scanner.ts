b57fd73c9c2e8421fb9de788c0b0aa13b11433bf
dominiksakic <dominik.sakic@yahoo.com>
2024-10-27 09:29:59 +0900

    chore: migrate to deno init

---
+++ Scanner.ts
@@ -0,0 +1,237 @@
+import Token from "./Token.ts";
+import TokenType from "./TokenType.ts";
+import Lox from "./main.ts";
+
+class Scanner {
+  private source: string;
+  private tokens: Token[] = [];
+  private start: number = 0;
+  private current: number = 0;
+  private line: number = 1;
+
+  keywords: Map<string, TokenType> = new Map([
+    ["and", TokenType.AND],
+    ["class", TokenType.CLASS],
+    ["else", TokenType.ELSE],
+    ["false", TokenType.FALSE],
+    ["for", TokenType.FOR],
+    ["fun", TokenType.FUN],
+    ["if", TokenType.IF],
+    ["nil", TokenType.NIL],
+    ["or", TokenType.OR],
+    ["print", TokenType.PRINT],
+    ["return", TokenType.RETURN],
+    ["super", TokenType.SUPER],
+    ["this", TokenType.THIS],
+    ["true", TokenType.TRUE],
+    ["var", TokenType.VAR],
+    ["while", TokenType.WHILE],
+  ]);
+
+  constructor(source: string) {
+    this.source = source;
+  }
+
+  scanTokens(): Token[] {
+    while (!this.isAtEnd()) {
+      this.start = this.current;
+      this.scanToken();
+    }
+
+    this.tokens.push(new Token(TokenType.EOF, "", null, this.line));
+    return this.tokens;
+  }
+
+  private scanToken() {
+    const c: string = this.advance();
+    switch (c) {
+      case "(":
+        this.addToken(TokenType.LEFT_BRACE);
+        break;
+      case ")":
+        this.addToken(TokenType.RIGHT_BRACE);
+        break;
+      case "{":
+        this.addToken(TokenType.LEFT_BRACE);
+        break;
+      case "}":
+        this.addToken(TokenType.RIGHT_BRACE);
+        break;
+      case ",":
+        this.addToken(TokenType.COMMA);
+        break;
+      case ".":
+        this.addToken(TokenType.DOT);
+        break;
+      case "-":
+        this.addToken(TokenType.MINUS);
+        break;
+      case "+":
+        this.addToken(TokenType.PLUS);
+        break;
+      case ";":
+        this.addToken(TokenType.SEMICOLON);
+        break;
+      case "*":
+        this.addToken(TokenType.STAR);
+        break;
+      case "!":
+        this.addToken(this.match(`=`) ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case "=":
+        this.addToken(
+          this.match(`=`) ? TokenType.EQUAL_EQUAL : TokenType.EQUAL,
+        );
+        break;
+      case "<":
+        this.addToken(this.match(`=`) ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case ">":
+        this.addToken(
+          this.match(`=`) ? TokenType.GREATER_EQUAL : TokenType.GREATER,
+        );
+        break;
+      case "/":
+        if (this.match("/")) {
+          while (this.peek() != "\n" && !this.isAtEnd()) this.advance();
+        } else if (this.match("*")) {
+          this.multiLineComment();
+        } else {
+          this.addToken(TokenType.SLASH);
+        }
+        break;
+      case " ":
+      case "\r":
+      case "\t":
+        break;
+      case "\n":
+        this.line++;
+        break;
+      case '"':
+        this.string();
+        break;
+      default:
+        if (this.isDigit(c)) {
+          this.number();
+        } else if (this.isAlpha(c)) {
+          this.identifier();
+        } else {
+          Lox.error(this.line, "Unexpected character.");
+          break;
+        }
+    }
+  }
+
+  private addToken(type: TokenType, literal: any = null) {
+    const text = this.source.substring(this.start, this.current);
+    this.tokens.push(new Token(type, text, literal, this.line));
+  }
+
+  private isAtEnd(): boolean {
+    return this.current >= this.source.length;
+  }
+
+  private advance(): string {
+    return this.source.charAt(this.current++);
+  }
+
+  private match(expected: string) {
+    if (this.isAtEnd()) return false;
+    if (this.source.charAt(this.current) != expected) return false;
+
+    this.current++;
+    return true;
+  }
+
+  private peek() {
+    if (this.isAtEnd()) return "\0";
+    return this.source.charAt(this.current);
+  }
+
+  private peekNext() {
+    if (this.current + 1 >= this.source.length) return "\0";
+    return this.source.charAt(this.current + 1);
+  }
+
+  private identifier() {
+    while (this.isAlphaNumeric(this.peek())) this.advance();
+
+    const text: string = this.source.substring(this.start, this.current);
+    const type: TokenType = this.keywords.get(text) ?? TokenType.IDENTIFIER;
+    this.addToken(type);
+  }
+
+  private string() {
+    while (this.peek() !== '"' && !this.isAtEnd()) {
+      if (this.peek() === "\n") this.line++;
+      this.advance();
+    }
+    if (this.isAtEnd()) {
+      Lox.error(this.line, "Unterminated string.");
+      return;
+    }
+
+    this.advance();
+
+    const value: string = this.source.substring(
+      this.start + 1,
+      this.current - 1,
+    );
+    this.addToken(TokenType.STRING, value);
+  }
+
+  private number() {
+    while (this.isDigit(this.peek())) this.advance();
+
+    if (this.peek() === "." && this.isDigit(this.peekNext())) {
+      this.advance();
+      while (this.isDigit(this.peek())) this.advance();
+    }
+
+    this.addToken(
+      TokenType.NUMBER,
+      parseFloat(this.source.substring(this.start, this.current)),
+    );
+  }
+
+  private isDigit(char: string) {
+    return char >= "0" && char <= "9";
+  }
+
+  private isAlpha(char: string) {
+    return (
+      (char >= "a" && char <= "z") ||
+      (char >= "A" && char <= "Z") ||
+      char === "_"
+    );
+  }
+
+  private isAlphaNumeric(char: string) {
+    return this.isAlpha(char) || this.isDigit(char);
+  }
+
+  private multiLineComment() {
+    let nestedComment: number = 1;
+
+    while (nestedComment > 0 && !this.isAtEnd()) {
+      if (this.peek() === "\n") this.line++;
+
+      if (this.peek() === "/" && this.peekNext() === "*") {
+        this.advance();
+        this.advance();
+        nestedComment++;
+      } else if (this.peek() === "*" && this.peekNext() === "/") {
+        this.advance();
+        this.advance();
+        nestedComment--;
+      } else {
+        this.advance();
+      }
+    }
+    if (nestedComment > 0) {
+      Lox.error(this.line, "Unterminated Comment");
+    }
+  }
+}
+
+export default Scanner;
---
+++ main.ts
@@ -0,0 +1,62 @@
+import Scanner from "./Scanner.ts";
+import Token from "./Token.ts";
+
+class Lox {
+  static hadError = false;
+
+  static async main(args: string[]) {
+    if (args.length > 1) {
+      console.log("Usage: tlox [script]");
+      Deno.exit(64);
+    } else if (args.length === 1) {
+      const filePath = args[0];
+      try {
+        const data = await Deno.readTextFile(filePath);
+        this.run(data);
+      } catch (error) {}
+      console.log("running file");
+    } else {
+      this.runPrompt();
+    }
+  }
+
+  static async runPrompt(): Promise<void> {
+    const decoder = new TextDecoder();
+    const encoder = new TextEncoder();
+    const buffer = new Uint8Array(1024);
+
+    while (true) {
+      await Deno.stdout.write(encoder.encode(">"));
+
+      const n = <number>await Deno.stdin.read(buffer);
+      if (n === 1) break;
+
+      const input = decoder.decode(buffer.subarray(0, n)).trim();
+      if (input === "exit") break;
+
+      this.run(input);
+      this.hadError = false;
+    }
+    Deno.exit(64);
+  }
+
+  static run(source: string): void {
+    const scanner = new Scanner(source);
+    const tokens: Token[] = scanner.scanTokens();
+
+    for (const token of tokens) {
+      console.log(token);
+    }
+  }
+  static error(line: number, message: string): void {
+    this.report(line, "", message);
+  }
+
+  static report(line: number, where: string, message: string): void {
+    console.error(`[line ${line}] Error ${where}: ${message}`);
+    this.hadError = true;
+  }
+}
+
+export default Lox;
+Lox.main(Deno.args);
